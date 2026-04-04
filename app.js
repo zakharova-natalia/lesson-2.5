cda3b47723ee18897a34a366229e07c52911ad0e
N Z <belkaznn@gmail.com>
2026-04-04 23:07:32 +0200

    Выполнено задание 2.4 PRO

--- app.js
+++ app.js
@@ -40,3 +40,57 @@ console.log("Адаптивность:", responsive);
 console.log("Общая стоимость проекта:", fullPrice);
 console.log("Название проекта:", titleProject);
 console.log("Типы экранов:", screensValue);
+
+const getAllServicePrices = function () {
+  return servicePrice1 + servicePrice2;
+};
+
+let allServicePrices = getAllServicePrices();
+console.log(allServicePrices);
+
+function getFullPrice(allServicePrices, screenPrice) {
+  return allServicePrices + screenPrice;
+}
+
+fullPrice = getFullPrice(allServicePrices, screenPrice);
+
+console.log("Полная стоимость проекта:" + fullPrice);
+
+function getTitle() {
+  return (
+    titleProject.charAt(0).toUpperCase() + titleProject.slice(1).toLowerCase()
+  );
+}
+
+titleProject = getTitle();
+
+console.log("Название проекта после обработки:", titleProject);
+
+function getServicePercentPrices() {
+  return Math.round(fullPrice - fullPrice * (percentage / 100));
+}
+
+servicePercentPrice = getServicePercentPrices();
+
+console.log(
+  "Итоговая сумма после вычета процента подрядчику:",
+  servicePercentPrice,
+);
+
+function getRollbackMessage() {
+  if (fullPrice > 50000) {
+    console.log("сделаем скидку в 10%");
+  } else if (fullPrice === 50000 || fullPrice === 20000) {
+    console.log("Вы попали точно в границу ценового диапазона!");
+  } else if (fullPrice > 20000 && fullPrice < 50000) {
+    console.log("сделаем скидку 5%");
+  } else if (fullPrice > 0 && fullPrice < 20000) {
+    console.log("Скидка не предусмотрена");
+  } else if (fullPrice === 0) {
+    console.log("цена не указана");
+  } else if (fullPrice < 0) {
+    console.log("Что то пошло не так");
+  }
+}
+
+getRollbackMessage(fullPrice);
