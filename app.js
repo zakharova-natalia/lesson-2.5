11f90567ca0969188dedebac392dc58e7a517453
N Z <belkaznn@gmail.com>
2026-06-13 12:33:47 +0200

    добавила все файлы по уроку 2.5

--- app.js
+++ app.js
@@ -1,96 +1,90 @@
+"use strict";
+
+const isNumber = function (n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+};
+
 let titleProject = prompt("Название проекта?");
 let screensValue = prompt(
   "Типы экранов? (шаблонные, с уникальным дизайном, с анимациями)",
 );
 let responsive = confirm("Нужен ли респонсивный сайт?");
-let screenPrice = 2000;
+let screenPrice;
 let percentage = 15;
 
-let service1 = prompt("Какой сервис нужен?");
-let servicePrice1 = +prompt("Сколько это будет стоить?");
-let service2 = prompt("Какой еще сервис тебе нужен?");
-let servicePrice2 = +prompt("Сколько будет стоить этот второй сервис?");
-let fullPrice = screenPrice + servicePrice1 + servicePrice2;
-let servicePercentPrice = Math.round(
-  fullPrice - fullPrice * (percentage / 100),
-);
-
-console.log(
-  "Итоговая сумма после вычета процента подрядчику:" + servicePercentPrice,
-);
-
-if (fullPrice > 50000) {
-  console.log("сделаем скидку в 10%");
-} else if (fullPrice === 50000 || fullPrice === 20000) {
-  console.log("Вы попали точно в границу ценового диапазона!");
-} else if (fullPrice > 20000 && fullPrice < 50000) {
-  console.log("сделаем скидку 5%");
-} else if (fullPrice > 0 && fullPrice < 20000) {
-  console.log("Скидка не предусмотрена");
-} else if (fullPrice === 0) {
-  console.log("цена не указана");
-} else if (fullPrice < 0) {
-  console.log("Что то пошло не так");
+while (
+  !isNumber(screenPrice) ||
+  screenPrice === null ||
+  screenPrice.trim() === ""
+) {
+  screenPrice = prompt("Сколько будет стоить верстка экранов?");
 }
-
-console.log("Название проекта:", titleProject);
-console.log("Типы экранов:", screensValue);
-
-console.log("Адаптивность:", responsive);
-console.log("Общая стоимость проекта:", fullPrice);
-console.log("Название проекта:", titleProject);
-console.log("Типы экранов:", screensValue);
+screenPrice = +screenPrice; // Переводим в числовой тип
 
 const getAllServicePrices = function () {
-  return servicePrice1 + servicePrice2;
+  let sum = 0;
+
+  for (let i = 0; i < 2; i++) {
+    let serviceName = prompt("Какой сервис нужен?");
+    let servicePrice = "";
+
+    while (
+      !isNumber(servicePrice) ||
+      servicePrice === null ||
+      servicePrice.trim() === ""
+    ) {
+      servicePrice = prompt("Сколько это будет стоить?");
+    }
+
+    sum += +servicePrice; // Прибавляем к общей сумме
+  }
+
+  return sum;
 };
 
 let allServicePrices = getAllServicePrices();
-console.log(allServicePrices);
 
 function getFullPrice(allServicePrices, screenPrice) {
   return allServicePrices + screenPrice;
 }
 
-fullPrice = getFullPrice(allServicePrices, screenPrice);
-
-console.log("Полная стоимость проекта:" + fullPrice);
+let fullPrice = getFullPrice(allServicePrices, screenPrice);
 
 function getTitle() {
   return (
-    titleProject.charAt(0).toUpperCase() + titleProject.slice(1).toLowerCase()
+    titleProject.trim().charAt(0).toUpperCase() +
+    titleProject.trim().slice(1).toLowerCase()
   );
 }
 
 titleProject = getTitle();
 
-console.log("Название проекта после обработки:", titleProject);
-
 function getServicePercentPrices() {
   return Math.round(fullPrice - fullPrice * (percentage / 100));
 }
 
-servicePercentPrice = getServicePercentPrices();
+let servicePercentPrice = getServicePercentPrices();
 
-console.log(
-  "Итоговая сумма после вычета процента подрядчику:",
-  servicePercentPrice,
-);
-
-function getRollbackMessage() {
-  if (fullPrice > 50000) {
-    console.log("сделаем скидку в 10%");
-  } else if (fullPrice === 50000 || fullPrice === 20000) {
-    console.log("Вы попали точно в границу ценового диапазона!");
-  } else if (fullPrice > 20000 && fullPrice < 50000) {
-    console.log("сделаем скидку 5%");
-  } else if (fullPrice > 0 && fullPrice < 20000) {
-    console.log("Скидка не предусмотрена");
-  } else if (fullPrice === 0) {
-    console.log("цена не указана");
-  } else if (fullPrice < 0) {
-    console.log("Что то пошло не так");
+function getRollbackMessage(price) {
+  if (price > 50000) {
+    return "Сделаем скидку в 10%";
+  } else if (price === 50000 || price === 20000) {
+    return "Вы попали точно в границу ценового диапазона!";
+  } else if (price > 20000 && price < 50000) {
+    return "Сделаем скидку 5%";
+  } else if (price > 0 && price < 20000) {
+    return "Скидка не предусмотрена";
+  } else if (price === 0) {
+    return "Цена не указана";
+  } else {
+    return "Что-то пошло не так";
   }
 }
 
-getRollbackMessage(fullPrice);
+console.log("Название проекта:", titleProject);
+console.log("Типы экранов:", screensValue);
+console.log("Адаптивность:", responsive);
+console.log("Общая стоимость проекта (fullPrice):", fullPrice);
+console.log("Стоимость доп. услуг (allServicePrices):", allServicePrices);
+console.log("Итоговая сумма после отката подрядчику:", servicePercentPrice);
+console.log(getRollbackMessage(fullPrice));
